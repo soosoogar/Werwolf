21f4d1b55bc5c576c4ed307883a8e7c8f40e5c8a
soosoogar <149917228+soosoogar@users.noreply.github.com>
2023-11-05 16:39:09 +0900

    Add files via upload

---
+++ Werwolf-main/werwolf/GameObject.gd
@@ -0,0 +1,15 @@
+extends Node
+
+#static func People(): #사람
+#static func Ffraud(): #사기꾼
+#static func Kkutulu(): #꾸투루
+#static func Tutelar(): #수호자
+#static func Researcher(): #연구자
+#static func Hunter(): #헌터
+#static func Witch(): #마녀
+#static func Fanatic(): #광신도
+#static func Cupid(): #큐피트
+#static func Police(): #경찰
+#static func Wolf(): #늑대
+#static func GameRule():
+#static func GameObject():
